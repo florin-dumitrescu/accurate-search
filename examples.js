44064fa9d4d073174307ef7a100fa1710938938d
Florin <florin.dumitrescu@gmail.com>
2020-01-31 22:21:43 +0200

    Update examples.js

--- examples.js
+++ examples.js
@@ -1,9 +1,10 @@
-const AccurateSearch = require('./index.min.js')
+const AccurateSearch = require('./AccurateSearch.js')
 
-let titles = ['The Lighthouse', 'Marriage Story', 'The Irishman', 'Transit', 'A Hidden Life', 'Little Women',
+let movies = ['The Lighthouse', 'Marriage Story', 'The Irishman', 'Transit', 'A Hidden Life', 'Little Women',
 	'Uncut Gems', 'Under the Silver Lake', 'The Beach Bum', 'Gloria Bell', 'An Elephant Sitting Still',
 	'Portrait of a Lady on Fire', 'Apollo 11', 'The Souvenir', 'Pain and Glory', 'Parasite'
 ]
+let ratings = [8.6, 8.1, 8.0]
 
 
 console.log('\nSearch: accurate, also inside words (default)')
@@ -11,15 +12,32 @@ console.log('\nSearch: accurate, also inside words (default)')
 //Initialize search
 let accurateSearch = new AccurateSearch()
 //Add data
-for (let i = 0; i < titles.length; i++) {
-	accurateSearch.addText(i, titles[i])
+for (let i = 0; i < movies.length; i++) {
+	accurateSearch.addText(i, movies[i])
 }
 //Search
 let t = Date.now()
 let foundIds = accurateSearch.search('a')
 console.log(Date.now() - t + ' ms')
 //Show results
-for (let id of foundIds) console.log(titles[id])
+for (let id of foundIds) console.log(movies[id])
+
+
+console.log('\nSearch with ratings')
+
+//Initialize search
+accurateSearch = new AccurateSearch()
+
+//Add data
+for (let i = 0; i < movies.length; i++) {
+	accurateSearch.addText(i, movies[i], ratings[i])
+}
+
+//Search
+foundIds = accurateSearch.search('e')
+
+//Show results
+for (let id of foundIds) console.log(movies[id])
 
 
 console.log('\nSearch: accurate, not inside words')
@@ -29,15 +47,15 @@ accurateSearch = new AccurateSearch({
 	insideWords: false
 })
 //Add data
-for (let i = 0; i < titles.length; i++) {
-	accurateSearch.addText(i, titles[i])
+for (let i = 0; i < movies.length; i++) {
+	accurateSearch.addText(i, movies[i])
 }
 //Search
 t = Date.now()
 foundIds = accurateSearch.search('a')
 console.log(Date.now() - t + ' ms')
 //Show results
-for (let id of foundIds) console.log(titles[id])
+for (let id of foundIds) console.log(movies[id])
 
 
 console.log('\nSearch: speed, also inside words')
@@ -47,15 +65,15 @@ accurateSearch = new AccurateSearch({
 	engine: 'speed'
 })
 //Add data
-for (let i = 0; i < titles.length; i++) {
-	accurateSearch.addText(i, titles[i])
+for (let i = 0; i < movies.length; i++) {
+	accurateSearch.addText(i, movies[i])
 }
 //Search
 t = Date.now()
 foundIds = accurateSearch.search('a')
 console.log(Date.now() - t + ' ms')
 //Show results
-for (let id of foundIds) console.log(titles[id])
+for (let id of foundIds) console.log(movies[id])
 
 
 console.log('\nSearch: speed, not inside words')
@@ -66,12 +84,29 @@ accurateSearch = new AccurateSearch({
 	insideWords: false
 })
 //Add data
-for (let i = 0; i < titles.length; i++) {
-	accurateSearch.addText(i, titles[i])
+for (let i = 0; i < movies.length; i++) {
+	accurateSearch.addText(i, movies[i])
 }
 //Search
 t = Date.now()
 foundIds = accurateSearch.search('a')
 console.log(Date.now() - t + ' ms')
 //Show results
-for (let id of foundIds) console.log(titles[id])
+for (let id of foundIds) console.log(movies[id])
+
+
+console.log('\nSearch suggestions')
+
+//Initialize search
+accurateSearch = new AccurateSearch()
+
+//Add data
+for (let i = 0; i < movies.length; i++) {
+	accurateSearch.addText(i, movies[i])
+}
+//Search
+t = Date.now()
+let suggestions = accurateSearch.suggestions('l')
+console.log(Date.now() - t + ' ms')
+//Show results
+for (let s of suggestions) console.log(s)
